9f342b7f3630981c26f556e332e02d11191364e8
Lenish K J <lenish.kj@flytxt.com>
2024-06-11 16:40:28 +0530

    Updated theme

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const navOffset = nav.offsetTop;
 
 function handleScroll() {
     if (window.pageYOffset >= navOffset) {
-        nav.classList.add('fixed-top');
+        nav.classList.add('nav-fixed');
     } else {
-        nav.classList.remove('fixed-top');
+        nav.classList.remove('nav-fixed');
     }
 }
 window.addEventListener('scroll', handleScroll);
